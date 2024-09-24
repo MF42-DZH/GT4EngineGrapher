705281ce2192298ba0ed79808ca0d0f1d6819a76
Azullia <crashmacompilers@gmail.com>
2024-09-24 22:54:00 +0100

    Smoothed out lines again.

--- src/main/scala/gtenginegrapher/wrappers/EngineGraph.scala
+++ src/main/scala/gtenginegrapher/wrappers/EngineGraph.scala
@@ -3,17 +3,13 @@ package gtenginegrapher.wrappers
 import gtenginegrapher.schema.{Engine, SimpleEngine}
 
 class EngineGraph(engine: SimpleEngine) {
-  implicit private class Rounding(bd: BigDecimal) {
-    def rtt: BigDecimal = bd.setScale(2, BigDecimal.RoundingMode.DOWN)
-  }
-
   lazy val points: Seq[(Int, (BigDecimal, BigDecimal))] =
     (engine.idleRpm to engine.torquePoints.map(_._2).max by 10).map { rpm =>
       (
         rpm,
         (
-          engine.torqueAt(rpm).getOrElse(BigDecimal(0)).rtt,
-          engine.powerAt(rpm).getOrElse(BigDecimal(0)).rtt,
+          engine.torqueAt(rpm).getOrElse(BigDecimal(0)),
+          engine.powerAt(rpm).getOrElse(BigDecimal(0)),
         ),
       )
     }
@@ -27,11 +23,6 @@ class EngineGraph(engine: SimpleEngine) {
     val max = points.filter(_._1 < engine.revLimit).maxBy(p => (p._2._2, p._1))
     (max._1, max._2._2)
   }
-
-//  println(
-//    s"""PEAK TORQUE: $peakTorque -> ${engine.powerAt(peakTorque._1)}
-//       | PEAK POWER: $peakPower -> ${engine.torqueAt(peakPower._1)}
-//       |""".stripMargin)
 }
 
 object EngineGraph {
